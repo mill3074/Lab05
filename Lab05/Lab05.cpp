f2d08eed48f9e546663a668c91a074bd82da3f73
mill3074 <mill3074@purdue.edu>
2024-09-25 13:51:14 -0400

    Add project files.

---
+++ Lab05/Lab05.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	vector<char> cypher = { 'V','F','X','B','L','I','T','Z','J','R','P','H','D','K','N','O','W','S','G','U','Y','Q','M','A','C','E' };
+
+	string text;
+	getline(cin, text);
+
+	cout << text.length();
+}
+
+
